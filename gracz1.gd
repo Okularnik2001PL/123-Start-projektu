2e044b6cb01f510e08707f30825dbe9cefb02be9
Okularnik2001PL <44050332+Okularnik2001PL@users.noreply.github.com>
2024-08-14 23:34:39 +0200

    123-Start-projektu

--- gracz1.gd
+++ gracz1.gd
@@ -1,14 +1,27 @@
 extends CharacterBody3D
-
-
+var camera=1
+var prędkośc_obrotu=0.1
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x*prędkośc_obrotu))
+		get_node("Camera3D2").rotate_x(deg_to_rad(-event.relative.y*prędkośc_obrotu))
 
 func _physics_process(delta):
+	#print(str($Camera3D2/RayCast3D.get_collider()))
+	if Input.is_action_just_pressed("zmiana_kamery"):
+		camera=-camera
+		match camera:
+			-1:
+				$Camera3D2/Camera3D.current=true
+				$Camera3D2.current=false
+			1:
+				$Camera3D2/Camera3D.current=false
+				$Camera3D2.current=true
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
